12f1dbcfc073c163c6c43e5cf90f1aa5e3bd3a9d
infoxggole <xggole.info@gmail.com>
2026-06-23 15:39:52 +0400

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Reviews from '../components/Reviews';
+import Projects from '../components/Projects'; // ১. এখানে ইমপোর্ট করলেন
 
 interface HomePageProps {
   onContactClick: () => void;
@@ -10,8 +11,13 @@ export default function HomePage({ onContactClick }: HomePageProps) {
   return (
     <div>
       <Hero onContactClick={onContactClick} />
-      {/* এখানে প্রপসটি পাস করে দিন */}
+      
+      {/* Services এর সাথে onContactClick পাস করা হয়েছে */}
       <Services onContactClick={onContactClick} />
+      
+      {/* ২. এখানে প্রজেক্ট সেকশনটি বসালেন */}
+      <Projects />
+      
       <Reviews />
     </div>
   );
