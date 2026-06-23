5a75741a9e80550f03dd18f357a0efe010ed584d
infoxggole <xggole.info@gmail.com>
2026-06-23 12:29:34 +0400

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // এখানে আপনার সুপারবেস URL এবং ANON_KEY বসান
-const supabaseUrl = 'YOUR_SUPABASE_URL=https://uuapaszbwtjfsgqrzlbo.supabase.co';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1YXBhc3pid3RqZnNncXJ6bGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzY4MzksImV4cCI6MjA5NzUxMjgzOX0.zNSq_8LlQTEAXMODbRCGKHPEDgl0jC7f6AlaRkvq5zM';
+const supabaseUrl =https://uuapaszbwtjfsgqrzlbo.supabase.co';
+const supabaseAnonKey =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1YXBhc3pid3RqZnNncXJ6bGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzY4MzksImV4cCI6MjA5NzUxMjgzOX0.zNSq_8LlQTEAXMODbRCGKHPEDgl0jC7f6AlaRkvq5zM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
