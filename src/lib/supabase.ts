512dffb32647abce63f9471c12972019d73c215e
infoxggole <xggole.info@gmail.com>
2026-06-23 12:41:43 +0400

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uuapaszbwtjfsgqrzlbo.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1YXBhc3pid3RqZnNncXJ6bGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzY4MzksImV4cCI6MjA5NzUxMjgzOX0.zNSq_8LlQTEAXMODbRCGKHPEDgl0jC7f6AlaRkvq5zM';
+// এই লাইনগুলো .env ফাইল থেকে তথ্য তুলে আনবে
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
