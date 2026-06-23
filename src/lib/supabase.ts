260168ac2a8bae1cd9d7c7da0820ac50d276b151
infoxggole <xggole.info@gmail.com>
2026-06-23 12:32:13 +0400

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
-// src/lib/supabase.ts
+
 import { createClient } from '@supabase/supabase-js';
 
-// এখানে আপনার সুপারবেস URL এবং ANON_KEY বসান
 const supabaseUrl = 'https://uuapaszbwtjfsgqrzlbo.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1YXBhc3pid3RqZnNncXJ6bGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzY4MzksImV4cCI6MjA5NzUxMjgzOX0.zNSq_8LlQTEAXMODbRCGKHPEDgl0jC7f6AlaRkvq5zM';
 
